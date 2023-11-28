0a5bf4c01b846a9c41bc4d9f7b31e6b8827d9e32
H4RU77 <ilopez@cetys.edu.mx>
2023-11-27 18:58:37 -0800

    Version1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,154 @@
+package org.example;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static String encriptar (String frase,String clave){
+
+        //Declarar variables para encriptar FRASE
+        int l=frase.length();
+        char[] arrayFraseChar= new char[l];
+        int[] arrayFraseInt=new int[l];
+        String[] arrayFraseString = new String [l];
+        // Cambiar la frase de string a arreglo de caracteres
+        arrayFraseChar = frase.toCharArray();
+
+        //declarar variables para encriptar CLAVE
+        char[] arrayClaveChar= new char[l];
+        int[] arrayClaveInt=new int[l];
+        String[] arrayClaveString = new String [l];
+        // Cambiar la frase de string a arreglo de caracteres
+        arrayClaveChar = clave.toCharArray();
+
+
+        //  Para frase y clave:
+        //  Recorrer cada caracter, obtener valor ASCII y después su valor en número binario
+        // (terminamos con un arreglo de strings del codigo binario de cada letra)
+        for (int i=0; i<l; i++){
+            //FRASE
+            arrayFraseInt[i] = arrayFraseChar[i]; //Obtener valor ASCII
+            arrayFraseInt[i]= Integer.parseInt(Integer.toBinaryString(arrayFraseInt[i])); //Volver binario
+
+            arrayFraseString[i]= Integer.toString(arrayFraseInt[i]); //Guardar como array de strings
+
+
+            //CLAVE
+            arrayClaveInt[i] = arrayClaveChar[i]; //Obtener valor ASCII
+            arrayClaveInt[i]= Integer.parseInt(Integer.toBinaryString(arrayClaveInt[i])); //Volver binario
+
+            arrayClaveString[i]= Integer.toString(arrayClaveInt[i]); //Guardar como array de strings
+
+        }
+
+        //USAR XOR
+        System.out.println(Arrays.toString(arrayFraseInt));
+        System.out.println(Arrays.toString(arrayClaveInt));
+
+        int tamañoBloque = arrayFraseString[0].length();
+        System.out.println(tamañoBloque);
+        String[] arrayXOR= new String[l];
+        int[] arrayXORInt = new int[l];
+        char[] arrayXORChar = new char[l];
+        int numero = 0;
+
+        // ciclo for que recorre cada digito de frase y clave para compararlos
+        for (int bloque = 0; bloque<l; bloque++){
+            for (int digito=0; digito<tamañoBloque;digito++){
+                //Comparar digitos de frase y clave y hacer XOR
+                if(arrayClaveString[bloque].charAt(digito)==arrayFraseString[bloque].charAt(digito)) { //Si los digitos son iguales, el valor en la tabla XOR es "0"
+                    if(arrayXOR[bloque]==null){
+                        arrayXOR[bloque] ="0";
+                    }else{
+                        arrayXOR[bloque] +='0';
+                    }
+
+                }else{ // si los digitos son diferentes, el valor en la tabla XOR es "1"
+                    if(arrayXOR[bloque]==null){
+                        arrayXOR[bloque] ="1";
+                    }else{
+                        arrayXOR[bloque] +='1';
+                    }
+
+                }
+
+
+
+            }
+            arrayXORInt[bloque]=Integer.parseInt(arrayXOR[bloque],2); //pasarlo de binario a decimal
+            numero = arrayXORInt[bloque];
+            arrayXORChar[bloque] = (char)numero;
+
+        }
+        System.out.println(Arrays.toString(arrayXORInt));
+        //arrayXORInt= new int[]{40, 50, 60, 70}; // caso de prueba
+        // convertir el resultado XOR  de valor ASCII  a caracteres
+        int posicion= 0;
+        for(int n:arrayXORInt){
+            arrayXORChar[posicion]= (char)n;
+            posicion++;
+        }
+        System.out.println(Arrays.toString(arrayXORChar));
+
+
+        //Quitar comas, backets y espacios
+        String formattedString = Arrays.toString(arrayXORChar)
+                .replace(",", "")
+                .replace("[", "")
+                .replace("]", "")
+                .replace(" ", "")
+                .trim();
+
+
+        return formattedString;
+
+
+    }
+
+    public static void main(String[] args) {
+        int menu = 1;
+        String frase;
+        String clave;
+        Scanner scan = new Scanner(System.in);
+
+        while(menu != 0){
+
+
+            System.out.println("Elija una opción del menú (ingrese el numero de la accion que desea realizar): ");
+            System.out.println("0. Salir");
+            System.out.println("1. Encriptar una frase" );
+            System.out.println("2. Desencriptar una frase");
+            menu = scan.nextInt();
+
+            switch(menu){
+                case 0:
+                    break;
+                case 1:
+                    System.out.println("Escriba la frase que desea encriptar: ");
+                    scan.nextLine();
+                    frase = scan.nextLine();
+                    System.out.println("Escriba la clave que desea utilizar (debe ser de la misma longitud que la frase)");
+                    clave = scan.nextLine();
+                    System.out.println(encriptar(frase,clave));
+                    break;
+                case 2:
+                    System.out.println("Escriba la frase que desea desencriptar: ");
+                    scan.nextLine();
+                    frase = scan.nextLine();
+                    System.out.println("Escriba la clave que desea utilizar (debe ser de la misma longitud que la frase)");
+                    clave = scan.nextLine();
+                    System.out.println(encriptar(frase,clave));
+                    break;
+
+
+                default:
+                    System.out.println("Opción no válida");
+                    System.out.println("");
+            }
+        }
+
+
+
+
+    }
+}
