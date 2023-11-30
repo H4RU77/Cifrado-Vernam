fe59080ccfe23f340bcf77456441bbb5a6aa70b8
H4RU77 <ilopez@cetys.edu.mx>
2023-11-30 15:32:51 -0800

    Fixed non-printable characters

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class Main {
     public static String encriptar (String frase,String clave){
+        //Array non-printable characters
+        String[] nonPrint = {"NULL","STOH","STOT","ENOT","ENQY","ACKN","BELL","BACK","HORT","LIFE","VERT","FORM","CARR","SHIO","SHII","DATA","DEC1","DEC2","DEC3","DEC4","NEGA","SYNI","EOTB","CANC","ENDM","SUBS","ESCA","FILE","GRSE","RESE","UNSE","SPAC"};
 
         //Declarar variables para encriptar FRASE
         int l=frase.length();
@@ -13,13 +15,14 @@ public class Main {
         String[] arrayFraseString = new String [l];
         // Cambiar la frase de string a arreglo de caracteres
         arrayFraseChar = frase.toCharArray();
+        System.out.println(arrayFraseChar);
 
         //declarar variables para encriptar CLAVE
         char[] arrayClaveChar= new char[l];
         int[] arrayClaveInt=new int[l];
         String[] arrayClaveString = new String [l];
         // Cambiar la frase de string a arreglo de caracteres
-        arrayClaveChar = clave.toCharArray();
+        //arrayClaveChar = clave.toCharArray();
 
 
         //  Para frase y clave:
@@ -45,16 +48,16 @@ public class Main {
         System.out.println(Arrays.toString(arrayFraseInt));
         System.out.println(Arrays.toString(arrayClaveInt));
 
-        int tamañoBloque = arrayFraseString[0].length();
-        System.out.println(tamañoBloque);
+        int tamanioBloque = arrayFraseString[0].length();
+        System.out.println(tamanioBloque);
         String[] arrayXOR= new String[l];
         int[] arrayXORInt = new int[l];
-        char[] arrayXORChar = new char[l];
+        String[] arrayXORString = new String[l];
         int numero = 0;
 
         // ciclo for que recorre cada digito de frase y clave para compararlos
         for (int bloque = 0; bloque<l; bloque++){
-            for (int digito=0; digito<tamañoBloque;digito++){
+            for (int digito=0; digito<tamanioBloque;digito++){
                 //Comparar digitos de frase y clave y hacer XOR
                 if(arrayClaveString[bloque].charAt(digito)==arrayFraseString[bloque].charAt(digito)) { //Si los digitos son iguales, el valor en la tabla XOR es "0"
                     if(arrayXOR[bloque]==null){
@@ -76,8 +79,7 @@ public class Main {
 
             }
             arrayXORInt[bloque]=Integer.parseInt(arrayXOR[bloque],2); //pasarlo de binario a decimal
-            numero = arrayXORInt[bloque];
-            arrayXORChar[bloque] = (char)numero;
+
 
         }
         System.out.println(Arrays.toString(arrayXORInt));
@@ -85,14 +87,20 @@ public class Main {
         // convertir el resultado XOR  de valor ASCII  a caracteres
         int posicion= 0;
         for(int n:arrayXORInt){
-            arrayXORChar[posicion]= (char)n;
+            if(n<=32){ //check non-printable characters
+                arrayXORString[posicion]=nonPrint[n-1];
+            }else{
+                arrayXORString[posicion]= String.valueOf((char)n);
+
+            }
             posicion++;
+
         }
-        System.out.println(Arrays.toString(arrayXORChar));
+        System.out.println(Arrays.toString(arrayXORString));
 
 
         //Quitar comas, backets y espacios
-        String formattedString = Arrays.toString(arrayXORChar)
+        String formattedString = Arrays.toString(arrayXORString)
                 .replace(",", "")
                 .replace("[", "")
                 .replace("]", "")
